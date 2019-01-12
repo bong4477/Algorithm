08c394f81b11643d11cb10e6f2ac209b0a618997
bong4477 <bong4477@naver.com>
2019-01-13 01:51:08 +0900

    Add files via upload

---
+++ Brown_Red.java
@@ -0,0 +1,26 @@
+package Solution;
+
+import java.util.Arrays;
+
+public class Brown_Red {
+	public static int[] solution(int brown, int red) {
+		int[] answer = new int[2];
+		for(int i = 1; i <= red; i++) {
+			if(red % i == 0) {
+				if(i*2+(red/i)*2+4 == brown) {
+					answer[0] = (red/i) + 2;
+					answer[1] = i+2;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(24, 24)));
+
+	}
+
+}
